6d03e3d3b2ac0332ac954b34722edeb61b0fd1a7
Santiago <147957491+CapDenProgrammierer@users.noreply.github.com>
2024-10-28 15:51:00 -0600

    commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/pages/Home";
 import About from "../components/pages/About";
+import homeAdmin from  "../components/pages/UsuarioAdmin/homeAdmin.jsx";
 import Contact from "../components/pages/Contact";
 import PaginaOficialTransito from "../components/pages/UsuarioOficialTransito/index.jsx";
 import CrearMulta from "../components/pages/UsuarioOficialTransito/CrearMulta.jsx";
@@ -17,6 +18,7 @@ export const MainRouter = () => {
         <Route path="/Pagina-Oficial-Transito" element={<PaginaOficialTransito />} />
         <Route path="/Gestion-Multas" element={<GestionMultas />} />
         <Route path="/Crear-Multa" element={<CrearMulta />} />
+        <Route path="/homeAdmin" element={<homeAdmin />} />
       </Routes>
     </BrowserRouter>
   );
