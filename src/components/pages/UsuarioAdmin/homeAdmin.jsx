76a1adb44bd2b350f5fb188e5def7c4c323b61ec
Santiago <147957491+CapDenProgrammierer@users.noreply.github.com>
2024-10-27 23:55:49 -0600

    Paginas del Admin
    agregue algunas de las paginas de admin y lo que llevo hecho

---
+++ src/components/pages/UsuarioAdmin/homeAdmin.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ButtonLink from "../atoms/ButtonLink";
+import Button from "../atoms/Button";
+import {
+  InvitadoNavbar,
+  UsuarioNavbar,
+  AdminNavbar,
+  JuezNavbar,
+  OficialNavbar,
+  Navbar,
+} from "../layouts/Navbar";
+
+const Home = () => {
+  const handleClick = () => {
+    alert("Boton clickeado");
+  };
+
+  return (
+    <div>
+      <header>
+        <InvitadoNavbar />
+        <UsuarioNavbar />
+        <AdminNavbar />
+        <JuezNavbar />
+        <OficialNavbar />
+      </header>
+
+      <ButtonLink variant="primary" text="Iniciar Sesion" to="/" />
+      <ButtonLink variant="secondary" text="Consulta de Multas" to="/about" />
+      <ButtonLink variant="alternative" text="Registrate aqui" to="/contact" />
+      <ButtonLink variant="outline" text="Iniciar Sesion" to="/" />
+
+      <Button variant="primary" text="Boton 1" onClick={handleClick} />
+      <Button variant="secondary" text="Boton 2" onClick={handleClick} />
+      <Button variant="alternative" text="Boton 3" onClick={handleClick} />
+      <Button variant="outline" text="Boton 4" onClick={handleClick} />
+
+      <Button />
+    </div>
+  );
+};
+
+export default Home;
