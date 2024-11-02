d9a97c9031e203557def746f836769be2f318561
Jimmy Bogantes <jbogantesr@ucenfotec.ac.cr>
2024-11-01 21:28:05 -0600

    actualiza main

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ButtonLink from "../atoms/ButtonLink";
 import Button from "../atoms/Button";
+import { useState } from "react";
+import {getUsuarioPorCedula} from "../../services/usuarioService";
 import {
   InvitadoNavbar,
   UsuarioNavbar,
@@ -8,6 +10,7 @@ import {
   JuezNavbar,
   OficialNavbar,
 } from "../layouts/Navbar";
+import { set } from "date-fns";
 
 const Home = () => {
   const handleClick = () => {
@@ -18,6 +21,23 @@ const Home = () => {
 --------------> Meter aqui CSS y JavaScript
 
 */
+
+const [cedula, setCedula] = useState("");
+  const [usuario, setUsuario] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      setError(null); // Limpiar el mensaje de error previo
+      setCedula("518860349")
+      const userData = await getUsuarioPorCedula("618860349");
+      setUsuario(userData);
+      console.log(usuario);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <div>
       <header>
@@ -33,7 +53,7 @@ const Home = () => {
       <ButtonLink variant="alternative" text="Registrate aqui" to="/contact" />
       <ButtonLink variant="outline" text="Iniciar Sesion" to="/" />
 
-      <Button variant="primary" text="Boton 1" onClick={handleClick} />
+      <Button variant="primary" text="Boton 1" onClick={handleSearch} />
       <Button variant="secondary" text="Boton 2" onClick={handleClick} />
       <Button variant="alternative" text="Boton 3" onClick={handleClick} />
       <Button variant="outline" text="Boton 4" onClick={handleClick} />
