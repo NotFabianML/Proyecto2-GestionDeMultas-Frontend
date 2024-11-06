36214a256deb3e07444ce95b70b375f6031cdd38
Daniela <malvaradon@ucenfotec.ac.cr>
2024-11-05 18:22:21 -0600

    Mapa de calor

---
+++ src/components/pages/MapaCalor/MapaCalor.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './MapaCalor.css';
+import InvitadoNavbarTres from '../../layouts/Navbar/InvitadoNavbarTres.jsx';
+import Footer from '../../layouts/Footer.jsx';
+import MapaCalorImg from '../../../assets/MapaCalorImg.png';
+
+const MapaCalor = () => {
+    return (
+        <div className="mapa-calor-page">
+            <header>
+                <InvitadoNavbarTres />
+            </header>
+
+            <main className="mapa-calor-content">
+                <div className="texto-contenedor">
+                    <h1>Echa un vistazo a nuestro mapa de calor.</h1>
+                    <p>Aquí podrás visualizar información importante para los ciudadanos.</p>
+                </div>
+                <div className="imagen-contenedor">
+                    <img 
+                        src={MapaCalorImg} 
+                        alt="Mapa de calor"
+                        className="mapa-calor-image"
+                    />
+                </div>
+            </main>
+
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
+};
+
+export default MapaCalor;
