987092309d83cb221c3f5c37b884bfae9669532c
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-11-06 14:57:38 -0600

    update routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,69 +1,74 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../components/pages/Home";
-import About from "../components/pages/About";
-import InicioAdmin from "../components/pages/UsuarioAdmin/InicioAdmin.jsx";
-import Contact from "../components/pages/Contact";
-import PaginaOficialTransito from "../components/pages/UsuarioOficialTransito/index.jsx";
-import CrearMulta from "../components/pages/UsuarioOficialTransito/CrearMulta.jsx";
-import GestionMultas from "../components/pages/UsuarioOficialTransito/GestionMultas.jsx";
-import PaginaJuez from "../components/pages/UsuarioJuez/index.jsx";
-import DisputasJuez from "../components/pages/UsuarioJuez/DisputasJuez.jsx";
-import ResolverDisputa from "../components/pages/UsuarioJuez/ResolverDisputa.jsx";
-import Infraccion from "../components/pages/UsuarioAdmin/CatalogoInfracciones/infraccion.jsx";
-import MultasTabla from "../components/pages/UsuarioAdmin/Estad. Multas/multasTabla.jsx";
-import ListaUsuarios from "../components/pages/UsuarioAdmin/GestionUsuarios/listaUsuarios.jsx";
-import Roles from "../components/pages/UsuarioAdmin/GestionRoles/roles.jsx";
+
+import PaginaInicio from "../components/pages/PaginaInicio/PaginaInicio.jsx";
 import AdministrarPerfil from "../components/pages/UsuarioAdmin/GestionUsuarios/administarPerfil.jsx";
-import RegistrarUsuarioAdmin from "../components/pages/UsuarioAdmin/GestionUsuarios/registrarUsuarioAdmin.jsx";
-import MultasGrafico from "../components/pages/UsuarioAdmin/Estad. Multas/multasGrafico.jsx";
-import PasswordRecovery from "../components/pages/PasswordRecovery/PasswordRecovery.jsx";
 import InicioSesion from "../components/pages/InicioSesion/InicioSesion.jsx";
 import RegistroUsuario from "../components/pages/RegistroUsuario/RegistroUsuario.jsx";
-import PaginaInicio from "../components/pages/PaginaInicio/PaginaInicio.jsx";
+import PasswordRecovery from "../components/pages/PasswordRecovery/PasswordRecovery.jsx";
 import ConsultaPublica from "../components/pages/ConsultaPublica/ConsultaPublica.jsx";
+import MapaCalor from "../components/pages/MapaCalor/MapaCalor.jsx";
+
 import PaginaUsuarioFinal from "../components/pages/UsuarioFinal/index.jsx";
 import MisMultas from "../components/pages/UsuarioFinal/MisMultas.jsx";
 import MisDisputas from "../components/pages/UsuarioFinal/MisDisputas.jsx";
 import MisVehiculos from "../components/pages/UsuarioFinal/MisVehiculos.jsx";
-import MapaCalor from "../components/pages/MapaCalor/MapaCalor.jsx";
 
+import InicioAdmin from "../components/pages/UsuarioAdmin/InicioAdmin.jsx";
+import RegistrarUsuarioAdmin from "../components/pages/UsuarioAdmin/GestionUsuarios/registrarUsuarioAdmin.jsx";
+import ListaUsuarios from "../components/pages/UsuarioAdmin/GestionUsuarios/listaUsuarios.jsx";
+import Roles from "../components/pages/UsuarioAdmin/GestionRoles/roles.jsx";
+import GestionMultas from "../components/pages/UsuarioOficialTransito/GestionMultas.jsx";
+import MultasGrafico from "../components/pages/UsuarioAdmin/EstadoMultas/multasGrafico.jsx";
+import Infraccion from "../components/pages/UsuarioAdmin/CatalogoInfracciones/infraccion.jsx";
+
+import PaginaOficialTransito from "../components/pages/UsuarioOficialTransito/index.jsx";
+import CrearMulta from "../components/pages/UsuarioOficialTransito/CrearMulta.jsx";
+import MultasTabla from "../components/pages/UsuarioAdmin/EstadoMultas/multasTabla.jsx";
+
+import PaginaJuez from "../components/pages/UsuarioJuez/index.jsx";
+import DisputasJuez from "../components/pages/UsuarioJuez/DisputasJuez.jsx";
+import ResolverDisputa from "../components/pages/UsuarioJuez/ResolverDisputa.jsx";
 
 
 export const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/Pagina-Oficial" element={<PaginaOficialTransito />} />
-        <Route path="/Gestion-Multas" element={<GestionMultas />} />
-        <Route path="/Crear-Multa" element={<CrearMulta />} />
-        <Route path="/Pagina-Juez" element={<PaginaJuez />} />
-        <Route path="/Disputas-Juez" element={<DisputasJuez />} />
-        <Route path="/Resolver-Disputa" element={<ResolverDisputa />} />
-        <Route path="/InicioAdmin" element={<InicioAdmin />} />
-        <Route path="/Infraccion" element={<Infraccion />} />
-        <Route path="/MultasTabla" element={<MultasTabla />} />
-        <Route path="/ListaUsuarios" element={<ListaUsuarios />} />
-        <Route path="/Roles" element={<Roles />} />
-        <Route path="/AdministrarPerfil" element={<AdministrarPerfil />} />
-        <Route path="/RegistrarUsuarioAdmin" element={<RegistrarUsuarioAdmin />} />
-        <Route path="/MultasGrafico" element={<MultasGrafico />} />
+        {/* Usuario invitado */}
         <Route path="/" element={<PaginaInicio />} />
-        <Route path="/Password-Recovery" element={<PasswordRecovery />} />
-        <Route path="/Inicio-Sesion" element={<InicioSesion />} />
-        <Route path="/Registro-Usuario" element={<RegistroUsuario />} />
-        <Route path="/Consulta-Publica" element={<ConsultaPublica />} />
-        <Route path="/Pagina-Usuario" element={<PaginaUsuarioFinal />} />
-        <Route path="/Mis-Multas" element={<MisMultas />} />
-        <Route path="/Mis-Disputas" element={<MisDisputas />} />
-        <Route path="/Mis-Vehiculos" element={<MisVehiculos />} />
-        <Route path="/Mapa-Calor" element={<MapaCalor />} />
+        <Route path="/administrar-perfil" element={<AdministrarPerfil />} />
+        <Route path="/inicio-sesion" element={<InicioSesion />} />
+        <Route path="/registro-usuario" element={<RegistroUsuario />} />
+        <Route path="/password-recovery" element={<PasswordRecovery />} />
+        <Route path="/consulta-publica" element={<ConsultaPublica />} />
+        <Route path="/mapa-calor" element={<MapaCalor />} />
 
+        {/* Usuario Final */}
+        <Route path="/pagina-usuario" element={<PaginaUsuarioFinal />} />
+        <Route path="/mis-multas" element={<MisMultas />} />
+        <Route path="/mis-disputas" element={<MisDisputas />} />
+        <Route path="/mis-vehiculos" element={<MisVehiculos />} />
 
-        
+        {/* Administrador */}
+        <Route path="/inicio-admin" element={<InicioAdmin />} />
+        <Route path="/registrar-usuario-admin" element={<RegistrarUsuarioAdmin />} />
+        <Route path="/lista-usuarios" element={<ListaUsuarios />} />
+        <Route path="/roles" element={<Roles />} />
+        <Route path="/gestion-multas" element={<GestionMultas />} />
+        <Route path="/multas-grafico" element={<MultasGrafico />} />
+        <Route path="/infraccion" element={<Infraccion />} />
+
+        {/* Oficial de Tránsito */}
+        <Route path="/pagina-oficial" element={<PaginaOficialTransito />} />
+        <Route path="/crear-multa" element={<CrearMulta />} />
+        <Route path="/multas-tabla" element={<MultasTabla />} />
+
+        {/* Juez de Tránsito */}
+        <Route path="/pagina-juez" element={<PaginaJuez />} />
+        <Route path="/disputas-juez" element={<DisputasJuez />} />
+        <Route path="/resolver-disputa" element={<ResolverDisputa />} />
+
       </Routes>
     </BrowserRouter>
   );
