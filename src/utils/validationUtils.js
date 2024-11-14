541f73ed32ac52efa3462f26a1eac008f9f11d53
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-11-14 16:50:25 -0600

    Update validationUtils.js

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -9,3 +9,15 @@ export const validatePassword = (password) => {
     const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*]).{8,16}$/;
     return passwordRegex.test(password);
 };
+
+// Valida el formato de la cédula costarricense (9 dígitos)
+export const validateCedula = (cedula) => {
+    const cedulaRegex = /^\d{9}$/; // Ejemplo: 123456789
+    return cedulaRegex.test(cedula);
+};
+
+// Valida el formato del número de teléfono costarricense (8 dígitos, comienza con 2, 4, 6, 7 o 8)
+export const validateTelefono = (telefono) => {
+    const telefonoRegex = /^[24678]\d{7}$/; // Ejemplo: 88888888
+    return telefonoRegex.test(telefono);
+};
