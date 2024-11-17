5f003d723f415ae845e596a022fe5e5852737905
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-11-16 18:37:45 -0600

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -34,7 +34,7 @@ import ResolverDisputa from "../components/pages/UsuarioJuez/ResolverDisputa.jsx
 
 export const MainRouter = () => {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
         {/* Usuario invitado */}
@@ -73,6 +73,6 @@ export const MainRouter = () => {
         <Route path="/resolver-disputa" element={<ResolverDisputa />} />
 
       </Routes>
-    </BrowserRouter>
+    // </BrowserRouter>
   );
 };
